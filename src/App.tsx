ce743e62d869e05e5a158098479814e953e3ddaa
keitaroinoue6 <k25k.soccer@gmail.com>
2025-02-02 16:11:02 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from '@/components/ui/button'
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello, Tailwind CSS!
+      </h1>
+      <Button>Click me</Button>
+
+    </div>
+  )
+}
+
+export default App
